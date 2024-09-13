664d05ac7e8524db1cb7e659a84b270b6124867a
fazlulShanto <37170785+fazlulShanto@users.noreply.github.com>
2024-09-13 11:50:58 +0600

    update: react static file serve

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import {connectToMongoDB,dotenv,genericUtils} from './exports.js'
+import { connectToMongoDB, dotenv, genericUtils, express } from "./exports.js";
 import { fileURLToPath } from "url";
 import path from "path";
-import createExpressApp from './expressApp.js';
+import createExpressApp from "./expressApp.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,10 +9,19 @@ const __dirname = path.dirname(__filename);
 process.env.__dirname = __dirname;
 dotenv.config();
 
+const staticDir = path.join(__dirname, "..", "dist");
+const htmlEntryFile = path.join(__dirname, "..", "dist", "index.html");
+
 const startServer = async () => {
     console.clear();
     genericUtils.envValidator(process.env);
     const app = createExpressApp();
+
+    app.use(express.static(staticDir));
+    app.get("*", (req, res) => {
+        res.sendFile(htmlEntryFile);
+    });
+
     await connectToMongoDB();
     app.listen(process.env.PORT, () => {
         console.log(`✅ Last Updated: ${new Date().toLocaleString()}`);
@@ -21,4 +30,3 @@ const startServer = async () => {
 };
 
 startServer();
-
